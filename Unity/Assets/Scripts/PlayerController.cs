359d6661be5bc4bfaafb032654d980cdc1855a93
Ben-Fields <ben.fields.t@gmail.com>
2018-12-05 13:01:32 -0600

    Update thrust controls 9

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -75,8 +75,8 @@ public class PlayerController : MonoBehaviour {
     private float leftWingExtended = 1;
     private float rightWingExtended = 1;
     // Total lift under left/right wing
-    private float leftLift = 0;
-    private float rightLift = 0;
+    private float leftWingForce = 0;
+    private float rightWingForce = 0;
 	// Past tracker angle values
 	private float pLeftArmAngle = 0, pRightArmAngle = 0;
 
@@ -105,9 +105,9 @@ public class PlayerController : MonoBehaviour {
         // Forward vector
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 5, Color.green);
         // Left wing lift
-        Debug.DrawRay(leftLiftPoint.position, -leftLiftPoint.up * 0.5f * leftLift, Color.blue);
+        Debug.DrawRay(leftLiftPoint.position, -leftLiftPoint.up * 0.5f * leftWingForce, Color.blue);
         // Right wing lift
-        Debug.DrawRay(rightLiftPoint.position, -rightLiftPoint.up * 0.5f * rightLift, Color.blue);
+        Debug.DrawRay(rightLiftPoint.position, -rightLiftPoint.up * 0.5f * rightWingForce, Color.blue);
         // Velocity vectors
         Vector3 vel = rb.GetPointVelocity(leftLiftPoint.TransformPoint(leftLiftPoint.position));
         vel = rb.transform.InverseTransformDirection(vel); // (local space)
@@ -173,50 +173,53 @@ public class PlayerController : MonoBehaviour {
 
         /// Lift Generation
         // Reset lift at beginning of update
-        leftLift = 0;
-        rightLift = 0;
+        leftWingForce = 0;
+        rightWingForce = 0;
 
-        // Generate lift based on bird's forward speed, downward speed, and the amount its wings are extended
-        // (per-wing basis, so rotation matters)
-            // Left
-                // Velocity at left lift point
-                Vector3 leftVel = /*rb.GetPointVelocity(leftLiftPoint.TransformPoint(leftLiftPoint.position)) +*/ group.velocity;
-                leftVel = rb.transform.InverseTransformDirection(leftVel); // (local space)
+		// Generate lift based on bird's forward speed, downward speed, and the amount its wings are extended
+		// (per-wing basis, so rotation matters)
+			// Left
+				// Velocity at left lift point
+				Vector3 leftVel = /*rb.GetPointVelocity(leftLiftPoint.TransformPoint(leftLiftPoint.position)) +*/ group.velocity;
+				leftVel = rb.transform.InverseTransformDirection(leftVel); // (local space)
                 
-                // Counteract vertical velocity
-                float leftDrag = leftVel.y;
-                // Have a greater effect if falling downward
-                if (leftDrag < 0) leftDrag *= -2f;  //-3, -1
-                else leftDrag*= -0.5f;
+				// Counteract vertical velocity
+				float leftDrag = leftVel.y;
+				// Have a greater effect if falling downward
+				if (leftDrag < 0) leftDrag *= -2f;
+				else leftDrag*= -0.5f;
 
-                // Add lift if going forward quickly
-                float ll = leftVel.z;
-                if (ll < 0) ll = 0;
-
-                leftLift += leftWingExtended * (leftDrag*vertDragScale*0 + ll*liftScale);
+				// Add lift if going forward quickly
+				float leftLift = leftVel.z;
+				if (leftLift < 0) leftLift = 0;
             // Right
-                // Velocity at right lift point
-                Vector3 rightVel = /*rb.GetPointVelocity(rightLiftPoint.TransformPoint(rightLiftPoint.position)) +*/ group.velocity;
-                rightVel = rb.transform.InverseTransformDirection(rightVel); // (local space)
+				// Velocity at right lift point
+				Vector3 rightVel = /*rb.GetPointVelocity(rightLiftPoint.TransformPoint(rightLiftPoint.position)) +*/ group.velocity;
+				rightVel = rb.transform.InverseTransformDirection(rightVel); // (local space)
 
-                // Counteract vertical velocity
-                float rightDrag = rightVel.y;
-                // Have a greater effect if falling downward
-                if (rightDrag < 0) rightDrag *= -2f;
-                else rightDrag *= -0.5f;
+				// Counteract vertical velocity
+				float rightDrag = rightVel.y;
+				// Have a greater effect if falling downward
+				if (rightDrag < 0) rightDrag *= -2f;
+				else rightDrag *= -0.5f;
 
-                // Add lift if going forward quickly
-                float rl = rightVel.z;
-                if (rl < 0) rl = 0;
-
-                rightLift += rightWingExtended * (rightDrag*vertDragScale*0 + rl*liftScale);
+				// Add lift if going forward quickly
+				float rightLift = rightVel.z;
+				if (rightLift < 0) rightLift = 0;
+			// Apply the lift
+			if (applyLift) {
+				leftWingForce += leftWingExtended * (leftDrag*vertDragScale*0 + leftLift*liftScale);
+                rightWingForce += rightWingExtended * (rightDrag*vertDragScale*0 + rightLift*liftScale);
+			}
+			/// DEBUG - remove
+			print(leftWingExtended);
 
         /// Input Control - Wing Thrust
-        // Generate thrust based on flapping wings. Just add it to the lift vector(s) to be applied.
+        // Generate thrust based on flapping wings.
 		if (pLeftArmAngle - tracker.data.leftArmAngle > 0)
-			leftLift += flapThrustScale * (pLeftArmAngle - tracker.data.leftArmAngle);
+			leftWingForce += flapThrustScale * (pLeftArmAngle - tracker.data.leftArmAngle);
 		if (pRightArmAngle - tracker.data.rightArmAngle > 0)
-			rightLift += flapThrustScale * (pRightArmAngle - tracker.data.rightArmAngle);
+			rightWingForce += flapThrustScale * (pRightArmAngle - tracker.data.rightArmAngle);
 
         /// Rotational Dampening
         // Nudge bird rotation so it stays upright when no other forces are applied
@@ -226,10 +229,8 @@ public class PlayerController : MonoBehaviour {
         
         /// Move the Bird
         // Apply forces based on lift at left/right wing
-		if (applyLift) {
-			AddRelativeForceAtPosition(new Vector3(0, leftLift, 0), leftLiftPoint.position);
-			AddRelativeForceAtPosition(new Vector3(0, rightLift, 0), rightLiftPoint.position);
-		}
+		AddRelativeForceAtPosition(new Vector3(0, leftWingForce, 0), leftLiftPoint.position);
+		AddRelativeForceAtPosition(new Vector3(0, rightWingForce, 0), rightLiftPoint.position);
 
         // Keep depth (distance from camera) fixed; prevent drift in forward / backward direction
         rb.transform.localPosition = new Vector3(rb.transform.localPosition.x,rb.transform.localPosition.y,0);
