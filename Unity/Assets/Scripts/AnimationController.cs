d838ed0813ead81cba5645809fe9442a2c3258ca
Ben-Fields <ben.fields.t@gmail.com>
2018-12-06 12:50:49 -0600

    Bird wing animation (sync with user)
    Bird wings move according to user's arms

---
+++ Unity/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+	// Reference to user input data
+	private SensorInterface tracker;
+	// Reference to Animator
+	private Animator anim;
+
+	// Initialization
+	void Start () {
+		// Get the tracker / reference to sensor info
+		tracker = GameObject.Find("Tracker").GetComponent<SensorInterface>();
+		// Get a reference to the Animator component
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Update wing animation based on arm angles
+		anim.Play("Flap", 0, (90 - tracker.data.leftArmAngle)/180);
+		anim.Play("Flap", 1, (90 - tracker.data.rightArmAngle)/180);
+	}
+}
